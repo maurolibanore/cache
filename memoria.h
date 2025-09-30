f8f24a8af3ac01d0f3dc444a557942f338ec29d6
Mauro Libanore <162159200+maurolibanore@users.noreply.github.com>
2025-09-30 16:55:36 -0300

    primeira classe criada(testes)

---
+++ memoria.h
@@ -0,0 +1,32 @@
+#ifndef MEMORIA_H
+#define MEMORIA_H
+
+#include <string>
+
+class Memoria {
+protected:
+    std::string nome;
+    int latencia;
+    Memoria* proximoNivel; // Ponteiro para o próximo nível da hierarquia
+
+public:
+    // Construtor
+    Memoria(const std::string& nome, int latencia, Memoria* proximoNivel = nullptr)
+        : nome(nome), latencia(latencia), proximoNivel(proximoNivel) {}
+
+    // Destrutor virtual é essencial para classes base com funções virtuais
+    virtual ~Memoria() = default;
+
+    void setProximoNivel(Memoria* nivel) {
+        this->proximoNivel = nivel;
+    }
+
+    // --- Interface Comum (Métodos Virtuais Puros) ---
+    // Devem ser implementados por Cache e MemoriaPrincipal.
+    // Retornam a latência TOTAL da operação a partir deste nível.
+    virtual long long ler(int endereco) = 0;
+    virtual long long escrever(int endereco) = 0;
+    virtual void imprimirEstatisticas() const = 0;
+};
+
+#endif // MEMORIA_H
